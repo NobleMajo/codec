0aaa5e87926e8d1c0fac18ed579e9c67cf8fc8eb
majo418 <majo@coreunit.net>
2022-09-25 03:02:22 +0000

    Change: codeserver version and disable readme feature

--- system/readme.sh
+++ system/readme.sh
@@ -11,5 +11,9 @@ fi
 
 cp /etc/codec/readme.md /codec/.codec/readme.md
 
-echo "[CODEC][README]: Open readme file!"
-code /etc/codec/readme.md > /dev/null 2>&1
+#todo repair readme
+#echo "[CODEC][README]: Open readme file!"
+#code /etc/codec/readme.md > /dev/null 2>&1
+
+#Sep 25 02:40:00 ffbf60fe179d systemd[1]: codec.service: Main process exited, code=exited, status=1/FAILURE
+#Sep 25 02:40:00 ffbf60fe179d systemd[1]: codec.service: Failed with result 'exit-code'.
