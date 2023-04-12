7f55629540c0b587c655758b442dd2cb08c9f59e
root <majo>
2023-04-12 23:23:13 +0000

    Fix: new mod folders bash scripts

--- system/bash.sh
+++ system/bash.sh
@@ -4,7 +4,7 @@ source /etc/environment
 
 codec -w
 
-MODS_PATH="/codec/.codec/mods"
+MODS_PATH="/codec/.codec/enabled-mods"
 BASH_MOD_PATHS=("$(find $MODS_PATH -name "*.bash.sh")")
 
 for BASH_MOD_PATH in $BASH_MOD_PATHS; do
